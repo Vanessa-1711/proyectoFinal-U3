6d6aa088809222513645e48d0643660af455a024
Yanel-Mireles <2030055@upv.edu.mx>
2023-08-11 01:46:14 -0600

    avance cotizaciones

---
+++ database/migrations/2023_08_11_050110_create_cotizaciones_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCotizacionesTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('cotizaciones', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('producto_id');
+            $table->string('referencia')->unique();
+            $table->string('cliente');
+            $table->enum('estatus', ['enviada', 'pendiente', 'otro']);
+            $table->decimal('total', 8, 2);
+            $table->text('descripcion')->nullable();
+            $table->date('fecha');
+            $table->timestamps();
+
+            $table->foreign('producto_id')->references('id')->on('products')->onDelete('cascade');
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('cotizaciones');
+    }
+};
